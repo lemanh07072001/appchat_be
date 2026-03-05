93b9b8f91b9509113ae03d147727749ecdcf123f
Lemanh <lemanh.laravel@gmail.com>
2026-03-05 14:55:19 +0700

    up proxyvn

--- src/worker.module.ts
+++ src/worker.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/orders.schema';
 import { Partner, PartnerSchema } from './schemas/partners.schema';
 import { Service, ServiceSchema } from './schemas/services.schema';
+import { Proxy, ProxySchema } from './schemas/proxies.schema';
 import { RedisModule } from './redis/redis.module';
 import { ProxyProvidersModule } from './proxy-providers/proxy-providers.module';
 import { OrdersWorkerService } from './orders/orders.worker.service';
@@ -28,6 +29,7 @@ import { OrdersWorkerService } from './orders/orders.worker.service';
       { name: Order.name,   schema: OrderSchema },
       { name: Partner.name, schema: PartnerSchema },
       { name: Service.name, schema: ServiceSchema },
+      { name: Proxy.name,   schema: ProxySchema },
     ]),
 
     RedisModule,
